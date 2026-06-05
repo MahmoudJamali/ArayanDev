d997a98a9541a8644e86cfac5404c05d728e9a50
MahmoudJamali <mahmoudjamali1370@gmail.com>
2026-06-05 19:34:01 +0330

    FixedUserpanel

--- ArayanDev/Areas/User/ViewModels/UserDashboardViewModel.cs
+++ ArayanDev/Areas/User/ViewModels/UserDashboardViewModel.cs
@@ -3,32 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Enums;
 
 namespace UI.Areas.User.ViewModels;
-public class UserDashboardViewModel
-{
-    public Guid UserId { get; set; }
 
-    public string PhoneNumber { get; set; }
+    public class UserDashboardViewModel
+    {
+        public Guid UserId { get; set; }
 
-    public string? Name { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string PhoneNumber { get; set; }
 
-    public string? Family { get; set; }
+        public string NationalCode { get; set; }
+        public string City { get; set; }
+        public DateOnly BirthDay { get; set; }
+    public string Address { get; set; }
+    public string EducationDegree { get; set; }
+        public string Major { get; set; }
+        public string Email { get; set; }
 
-    public string? NationalCode { get; set; }
+        public List<UserCourseItemViewModel> Courses { get; set; }
+            = new();
+    }
 
-    public string? City { get; set; }
+    public class UserCourseItemViewModel
+    {
+        public Guid CourseId { get; set; }
 
-    public DateOnly BirthDay { get; set; }
+        public string Title { get; set; }
 
-    public string? EducationDegree { get; set; }
+        public decimal Price { get; set; }
 
-    public string? Major { get; set; }
+        public bool IsFree { get; set; }
 
-    public string? Address { get; set; }
+        public DateTime? StartDate { get; set; }
 
-    public string? Email { get; set; }
-}
+        public CourseType CourseType { get; set; }
+
+        public DateTime EnrollDate { get; set; }
+    }
+
 
 
 
--- Business/Handlers/Users/Queries/GetUserDashboardQuery.cs
+++ Business/Handlers/Users/Queries/GetUserDashboardQuery.cs
@@ -3,76 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Handlers.Users.Queries;
 using DataAccess.Abstract;
-
+using DataAccess.Concrete.Contexts;
 using MediatR;
+using DataAccess.Concrete.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Entities.Enums;
 
-namespace Business.Handlers.Users.Queries;
-public class UserDashboardViewModel
+
+namespace Business.Handlers.Users.Queries
 {
-    public Guid UserId { get; set; }
+    public class UserDashboardViewModel
+    {
+        public Guid UserId { get; set; }
 
-    public string PhoneNumber { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string PhoneNumber { get; set; }
 
-    public string? Name { get; set; }
+        public string NationalCode { get; set; }
+        public string City { get; set; }
+        public DateOnly BirthDay { get; set; }
+        public string Address { get; set; }
+        public string EducationDegree { get; set; }
+        public string Major { get; set; }
+        public string Email { get; set; }
 
-    public string? Family { get; set; }
+        public List<UserCourseItemViewModel> Courses { get; set; }
+            = new();
+    }
 
-    public string? NationalCode { get; set; }
+    public class UserCourseItemViewModel
+    {
+        public Guid CourseId { get; set; }
 
-    public string? City { get; set; }
+        public string Title { get; set; }
 
-    public DateOnly BirthDay { get; set; }
+        public decimal Price { get; set; }
 
-    public string? EducationDegree { get; set; }
+        public bool IsFree { get; set; }
 
-    public string? Major { get; set; }
+        public DateTime? StartDate { get; set; }
 
-    public string? Address { get; set; }
+        public CourseType CourseType { get; set; }
 
-    public string? Email { get; set; }
+        public DateTime EnrollDate { get; set; }
+    }
 }
+
+
 public class GetUserDashboardQuery : IRequest<UserDashboardViewModel>
 {
     public Guid UserId { get; set; }
 }
 
+
+
+
 public class GetUserDashboardQueryHandler
     : IRequestHandler<GetUserDashboardQuery, UserDashboardViewModel>
 {
-    private readonly IUserRepository _userRepository;
+    private readonly AppDbContext _context;
 
-    public GetUserDashboardQueryHandler(IUserRepository userRepository)
+    public GetUserDashboardQueryHandler(AppDbContext context)
     {
-        _userRepository = userRepository;
+        _context = context;
     }
 
     public async Task<UserDashboardViewModel> Handle(
         GetUserDashboardQuery request,
         CancellationToken cancellationToken)
     {
-        var user = await _userRepository.GetUserWithProfile(request.UserId);
+
+        var user = await _context.Users
+            .Include(x => x.Profile)
+            .FirstOrDefaultAsync(x => x.Id == request.UserId);
 
         if (user == null)
             return null;
+
+        var courses = await _context.CourseEnrollment
+            .Where(x => x.UserId == request.UserId)
+            .Include(x => x.Course)
+            .Select(x => new UserCourseItemViewModel
+            {
+                CourseId = x.CourseId,
+                Title = x.Course.Title,
+                Price = x.Course.Price,
+                IsFree = x.Course.IsFree,
+                StartDate = x.Course.StartDate,
+                CourseType = x.CourseType,
+                EnrollDate = x.EnrollDate
+            })
+            .ToListAsync();
 
         return new UserDashboardViewModel
         {
             UserId = user.Id,
             PhoneNumber = user.PhoneNumber,
 
-            Name = user.Profile?.Name,
-            Family = user.Profile?.Family,
-            NationalCode = user.Profile?.NationalCode,
-            City = user.Profile?.City,
-            BirthDay = user.Profile?.BirthDay ?? default,
-            EducationDegree = user.Profile?.EducationDegree,
-            Major = user.Profile?.Major,
-            Address = user.Profile?.Address,
-            Email = user.Profile?.Email
+            Name = user.Profile.Name,
+            Family = user.Profile.Family,
+            NationalCode = user.Profile.NationalCode,
+            City = user.Profile.City,
+            BirthDay = user.Profile.BirthDay,
+            EducationDegree = user.Profile.EducationDegree,
+            Major = user.Profile.Major,
+            Email = user.Profile.Email,
+            Address = user.Profile.Address,
+            Courses = courses
         };
     }
 }
+
+
 
 
 
